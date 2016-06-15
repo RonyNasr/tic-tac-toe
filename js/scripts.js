6507e56ec83477c72414b81a22d106582eb54015
Igor Seleznov and Rony Nasr <rony@>
2016-06-15 16:50:07 -0700

    modified front end logic

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ var win=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 
 
 var getImage = function (player){
-  if (player === 1){
+  if (player === 0){
     console.log("x");
     return "<div id='x'><img src='./img/x.png'></div>";
   }else {
@@ -15,13 +15,7 @@ var getImage = function (player){
 
 $(function () {
   $("div.square").click(function(){
-
-    var path = "." +  this.classList[2]? ( this.classList[0] + "." + this.classList[1] "." +this.classList[2]):this.classList[1]? ( this.classList[0] + "." +this.classList[1]): this.classList[0];
-console.log(path);
-    alert("." +  this.classList[2]? ( this.classList[0] + "." + this.classList[1] "." +this.classList[2])
-            :this.classList[1]? ( this.classList[0] + "." +this.classList[1]): this.classList[0]);
-
-    $("." + this.classList[0] + this.classList[1]? ("." + this.classList[1]):"" + this.classList[2]? ("." +this.classList[2]):"").append(getImage(1));
+    $("#"+this.id).append(getImage(turn));
 
   });
 });
